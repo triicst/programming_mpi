139cfabe129d7bf8844ffc0d29c34511d7562c99
triicst <tri.pham.hpc@gmail.com>
2020-04-02 22:13:05 +0700

    update work_size

--- mpi_send_and_receive/ping_pong.c
+++ mpi_send_and_receive/ping_pong.c
@@ -8,6 +8,13 @@ int main(int argc, char** argv) {
 
   MPI_Init(NULL, NULL);
 
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  if (world_size != 2){
+    fprintf(stderr, "World size must be two  for %s\n", argv[0]);
+    MPI_Abort(MPI_COMM_WORLD,1);
+  }
+
   int world_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
