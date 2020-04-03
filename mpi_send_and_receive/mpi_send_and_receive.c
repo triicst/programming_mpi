a2b8d803356dfcedced5f49521fc1af5e6c62990
triicst <tri.pham.hpc@gmail.com>
2020-04-03 15:01:24 +0700

    check_status_function

--- mpi_send_and_receive/mpi_send_and_receive.c
+++ mpi_send_and_receive/mpi_send_and_receive.c
@@ -1,7 +1,7 @@
 #include <mpi.h>
 #include <stdio.h>
-int main(int argc, char *argv[]) {
-  MPI_Init(NULL,NULL);
+int main(int argc, char** argv[]) {
+  MPI_Init(NULL, NULL);
 
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
